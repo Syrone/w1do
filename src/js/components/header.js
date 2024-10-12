b71898336dc56619aa835a4f69aa6ca147b2c21f
Andrew Vasilchenko <cazonove@gmail.com>
2024-10-13 00:07:29 +0300

    added page-header, profile, search

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,15 +1,25 @@
 const headerUnion = document.querySelector('.header-union')
+const headerNav = document.querySelector('.header-nav')
 const headerNavLinks = document.querySelectorAll('.header-nav .header-nav-link')
 
 function updateHeaderUnionPosition() {
 	headerNavLinks.forEach((link) => {
 		if (link.classList.contains('is-active')) {
-			const linkRect = link.getBoundingClientRect();
-			const leftOffset = linkRect.left + window.scrollX;
-			headerUnion.style.transform = `translateX(${leftOffset - 16}px)`;
+			const linkRect = link.getBoundingClientRect()
+			const leftOffset = linkRect.left + window.scrollX
+			headerUnion.style.transform = `translateX(${leftOffset - 16}px)`
 		}
-	});
+	})
 }
 
-document.addEventListener('DOMContentLoaded', updateHeaderUnionPosition);
-window.addEventListener('resize', updateHeaderUnionPosition);
+function getHeaderNavHeight() {
+	const headerNavHeight = headerNav.offsetHeight
+	document.documentElement.style.setProperty('--header-nav-height', `${headerNavHeight}px`)
+}
+
+document.addEventListener('DOMContentLoaded', updateHeaderUnionPosition)
+window.addEventListener('load', getHeaderNavHeight)
+window.addEventListener('resize', () => {
+	getHeaderNavHeight()
+	updateHeaderUnionPosition()
+})
