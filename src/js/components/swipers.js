2d01cd2b87b00848eb0e98aa2153b647655508ed
Andrew Vasilchenko <cazonove@gmail.com>
2024-10-15 14:37:32 +0300

    fix header; added banners; added setion-header

--- src/js/components/swipers.js
+++ src/js/components/swipers.js
@@ -1,10 +1,19 @@
 import Swiper from 'swiper'
 
 const swiperCardClass = document.querySelectorAll('.swiper-card')
+const swiperBannerClass = document.querySelectorAll('.swiper-banner')
 
 swiperCardClass?.forEach((element) => {
 	new Swiper(element, {
 		slidesPerView: 'auto',
+		spaceBetween: 4,
+	})
+})
+
+swiperBannerClass?.forEach((element) => {
+	new Swiper(element, {
+		initialSlide: 1,
+		slidesPerView: 1,
 		spaceBetween: 4,
 	})
 })
